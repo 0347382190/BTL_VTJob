5afea36cd4947b940be337f5b0deda709fb87ce7
h.haqbhg@gmail.com <h.haqbhg@gmail.com>
2023-12-03 17:59:25 +0700

    cmt Admin cotroller

---
+++ BTL_VTJob/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_VTJob.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
